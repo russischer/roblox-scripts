634728e3288cb708daa511de358cbea86bd71c2d
russischer <kitisadev6@gmail.com>
2025-04-09 19:28:46 +0500

    Update and rename load-player-character.lua to Create player character.lua

---
+++ Create player character.lua
@@ -0,0 +1,3 @@
+id = 1
+m = game.Players:CreateHumanoidModelFromDescription(game.Players:GetHumanoidDescriptionFromUserId(id), Enum.HumanoidRigType.R6) 
+m.Parent = workspace
--- load-player-character.lua
+++
@@ -1,3 +0,0 @@
-id = 1
-m = game.Players:CreateHumanoidModelFromDescription(game.Players:GetHumanoidDescriptionFromUserId(1), Enum.HumanoidRigType.R6) 
-m.Parent = workspace
